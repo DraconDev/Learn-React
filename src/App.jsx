bc00aac2a7292303c98df05af925622c9b58a9f5
unknown <dracsharp@gmail.com>
2020-01-17 11:37:22 +0000

    done for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import {useSelector, useDispatch} from "react-redux";
+import {increment, decrement} from "./actions";
 
 function App() {
+  //CAN ACCESS ENTIRE STATE
+  const counter = useSelector( state => state.counter)
+  const isLogged = useSelector( state => state.isLogged)
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1> Counter {counter}</h1>
+      <button onClick={() => dispatch(increment(3))}>+</button>
+      <button onClick={() => dispatch(decrement(1))}>-</button>
+       {isLogged? <h3>Shouldn't see this</h3> : ""}
     </div>
   );
 }
